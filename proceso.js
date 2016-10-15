3086ad4bce4a4f3b366dc62ebf3a5e217226ae40
H3n1997 <henry_bm76@hotmail.com>
2016-10-14 22:50:15 -0500

    proceso de suma

--- proceso.js
+++ proceso.js
@@ -19,4 +19,54 @@ function altaAlumno(e){
 function comienzo(e){
 	e.preventDefault();
 	$('table').addClass('muestra');
+	$('div.res table tr td button#a1').on('click', pre1a);
+	$('div.res table tr td button#b2').on('click', pre2b);
+	$('div.res table tr td button#c3').on('click', pre3c);
+	$('div.res table tr td button#d4').on('click', pre4d);
+	$('div.res table tr td button#a5').on('click', pre5a);
+	$('div.res table tr td button#b6').on('click', pre6b);
+	$('div.res table tr td button#c7').on('click', pre7c);
+	$('div.res table tr td button#d8').on('click', pre8d);
+	$('div.res table tr td button#a9').on('click', pre9a);
+	$('div.res table tr td button#b10').on('click', pre10b);
+}
+function pre1a(){
+	var puntoA=1;
+	console.log(puntoA+'p1');
+}
+function pre2b(){
+	var puntoB=1;
+	console.log(puntoB+'p2');
+}
+function pre3c(){
+	var puntoC=1;
+	console.log(puntoC+'p3');
+}
+function pre4d(){
+	var puntoD=1;
+	console.log(puntoD+'p4');
+}
+function pre5a(){
+	var puntoAA=1;
+	console.log(puntoAA+'p5');
+}
+function pre6b(){
+	var puntoBB=1;
+	console.log(puntoBB+'p6');
+}
+function pre7c(){
+	var puntoCC=1;
+	console.log(puntoCC+'p7');
+}
+function pre8d(){
+	var puntoDD=1;
+	console.log(puntoDD+'p8');
+}
+function pre9a(){
+	var puntoAAA=1;
+	console.log(puntoAAA+'p9');
+}
+function pre10b(){
+	var puntoBBB=1;
+	console.log(puntoBBB+'p10');
 }
